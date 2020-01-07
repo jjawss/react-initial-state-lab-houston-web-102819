18fb04a5c25a2f91bf78a2c7ba1b673001bf2d74
David Jaworski <jaws360@gmail.com>
2020-01-07 16:28:43 -0600

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
 // your Bomb code here!
+import React from "react";
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+    // if (this.state.secondsLeft > 0) {
+    //   return `${this.state.secondsLeft} seconds before I go boom`;
+    // } else {
+    //   return "-0 seconds left before I go boom!";
+  }
+
+  render() {
+    if (this.state.secondsLeft > 0) {
+      return (
+        // setState = this.state.secondsLeft
+        <div>{`${this.state.secondsLeft} seconds le before I go boom!`}</div>
+      );
+    } else {
+      return <div>Boom!</div>;
+    }
+  }
+}
+
+export default Bomb;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ImageSlider from './ImageSlider';
-import Bomb from './Bomb';
+import React from "react";
+import ReactDOM from "react-dom";
+import ImageSlider from "./ImageSlider";
+import Bomb from "./Bomb";
 
 ReactDOM.render(
   <div>
-    <ImageSlider />
+    <ImageSlider
+    // prop1={"Some info"}
+    // prop2={"More info"}
+    // whateverMakesMostSenseToNameThis={"Some more info"}
+    />
     <Bomb initialCount={120} />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
